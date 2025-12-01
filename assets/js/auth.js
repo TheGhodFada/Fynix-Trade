720f8d0ed649a9388a37ab34325bd2df0b14fd6f
GhodFada <fynixtrade@gmail.com>
2025-12-01 21:16:49 +0100

    Update and rename auth.js to assets/js/auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -70,3 +70,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('loginForm');
   if (loginForm) loginForm.addEventListener('submit', login);
 });
+
